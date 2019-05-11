191a126a7f5871af13e66ee53d8b4c797b41c0bb
kenji matsuoka <egs@firespeed.org>
2019-05-11 19:04:12 +0900

    refactoring ObjectDotGraphic.

--- app/src/main/java/com/google/firebase/ml/md/objectdetection/ObjectDotGraphic.kt
+++ app/src/main/java/com/google/firebase/ml/md/objectdetection/ObjectDotGraphic.kt
@@ -27,7 +27,7 @@ import com.google.firebase.ml.md.camera.GraphicOverlay.Graphic
 import com.google.firebase.ml.md.R
 
 /** A dot to indicate a detected object used by multiple objects detection mode.  */
-internal class ObjectDotGraphic(overlay: GraphicOverlay, `object`: DetectedObject, private val animator: ObjectDotAnimator) : Graphic(overlay) {
+internal class ObjectDotGraphic(overlay: GraphicOverlay, detectedObject: DetectedObject, private val animator: ObjectDotAnimator) : Graphic(overlay) {
     private val paint: Paint
     private val center: PointF
     private val dotRadius: Int
@@ -35,20 +35,21 @@ internal class ObjectDotGraphic(overlay: GraphicOverlay, `object`: DetectedObjec
 
     init {
 
-        val box = `object`.boundingBox
+        val box = detectedObject.boundingBox
         center = PointF(
                 overlay.translateX((box.left + box.right) / 2f),
                 overlay.translateY((box.top + box.bottom) / 2f))
 
-        paint = Paint()
-        paint.style = Style.FILL
-        paint.color = Color.WHITE
+        paint = Paint().apply {
+            style = Style.FILL
+            color = Color.WHITE
+        }
 
         dotRadius = context.resources.getDimensionPixelOffset(R.dimen.object_dot_radius)
         dotAlpha = paint.alpha
     }
 
-    public override fun draw(canvas: Canvas) {
+    override fun draw(canvas: Canvas) {
         paint.alpha = (dotAlpha * animator.alphaScale).toInt()
         canvas.drawCircle(center.x, center.y, dotRadius * animator.radiusScale, paint)
     }
